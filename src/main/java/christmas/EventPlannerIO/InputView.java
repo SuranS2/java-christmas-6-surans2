90681ed1fc40d3686e8209e602d0def1f062a80e
Sur0n <skyzephyr333@gmail.com>
2023-11-15 08:05:36 +0900

    feat : write down InputView function 3

--- src/main/java/christmas/EventPlannerIO/InputView.java
+++ src/main/java/christmas/EventPlannerIO/InputView.java
@@ -1,14 +1,40 @@
 package christmas.EventPlannerIO;
+
+
+
 import camp.nextstep.edu.missionutils.Console;
+import christmas.EventPlannerIO.OutputView;
+
+import christmas.EventPlannerIO.InputViewValidator.*;
 
 public class InputView {
-    
-    public void readDate() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
+
+    private final OutputView printer;
+    private final InputViewValidator inputViewValidator;
+
+    public InputView() {
+        this.printer = new OutputView();
+        this.inputViewValidator = new InputViewValidator();
+    }
+
+//    public static final Pattern NUMBER_FORMAT = Pattern.compile("^-?\\d+$");
+//    public static final Pattern INPUT_ANSWER_LOTTO_NUMBERS_FORMAT =
+//            Pattern.compile("^" + "-?\\d+,".repeat(Lotto.LOTTO_SIZE - 1) + "-?\\d+$");
+//    public static final String NUMBERS_DELIMITER = ",";
+
+    public int readDate() {
+        while (true) {
+            try {
+                printer.printAskVisitDate();
+                String inputDate = Console.readLine();
+                return inputViewValidator.validateDate(inputDate);
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                printer.printErrorMessage(e);
+            }
+        }
         // ...
-        
         //int값 리턴
     }
+
     // ...
 }
